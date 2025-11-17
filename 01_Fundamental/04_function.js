95a63b03fb4ea3b7477574f1bd1d43c42c7499ec
narasena <cursedsena@gmail.com>
2025-11-17 14:55:32 +0700

    day05.5

--- 01_Fundamental/04_function.js
+++ 01_Fundamental/04_function.js
@@ -249,9 +249,67 @@ console.log(trianglePattern(4))
 // 07 08 09 10
 // ● Parameters : height → triangle height
 
-// ● Create a function that can loop the number of times according to the input we provide, and will 
+// 1. Angka pada isi array terus berlanjut ( +1 dari nilai terakhir)
+// 2. Panjang array sesuai dengan tinggi array
+// 3. Selama panjang array < tinggi array, dia akan bertambah +1 (push nilai terakhir +1) 
+
+function trianglePatterVerTwo(height){
+    let array = []
+    let lastNum = 0
+    for(i=1; i<=height; i++){    // i = 1                 i = 2                                                 i = 3
+        while(array.length < i){ // array.length = 0 < 1  array.lnegth = 0 < 2         array.length = 1 < 2
+            lastNum++            // lastNum = 0 + 1 = 1   lastNum = 1 + 1 = 2          lastNum = 2 + 1 = 3
+            array.push(lastNum)  // [] push(1) => [1]     [] push(2) => [2]            [2] push(3) => [2,3]
+        }                        //                       while cek array.length: 1    cek length : 2
+        console.log(array)       // print array = [1]                                  print array [2,3]
+        array = []                // [1] => []                                         [2,3] => []
+    }
+}
+
+trianglePatterVerTwo(3)
+
+// ● Create a function that can loop the number of times according to the input we provide, 
+// and will 
 // replace multiples of 3 with "Fizz", multiples of 5 with "Buzz", multiples of 3 and 5 with 
 // "FizzBuzz".
 // ● Parameters : n → total looping
 // ○ Example: n = 6 →1, 2, Fizz, 4, Buzz, Fizz
 // ○ Example: n = 15 → 1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 12, 13, 14, FizzBuzz 
+
+function fizzBuzz(n){
+    let array = []
+    for(i=1; i<=n; i++){
+        if(i%3===0 && i%5 ===0 ){
+            array.push("FizzBuzz")
+        } else if (i%5 === 0){
+            array.push("Buzz")
+        } else if (i%3 === 0 ){
+            array.push("Fizz")
+        } else {
+            array.push(i)
+        }
+    }
+    return array
+}
+
+console.log(fizzBuzz(16))
+
+// Create a function to calculate Body Mass Index (BMI)
+// ● Formula : BMI = weight (kg) / (height (meter))²
+// ● Parameters : weight & height
+// ● Return values :
+// ○ < 18.5 return “less weight”
+// ○ 18.5 - 24.9 return “ideal”
+// ○ 25.0 - 29.9 return “overweight”
+// ○ 30.0 - 39.9 return “very overweight”
+// ○ > 39.9 return “obesity”z
+
+// Write a function to remove all odd numbers in an array and return a new array that contains even 
+// numbers only
+// ○ Example : [1,2,3,4,5,6,7,8,9,10] → [2,4,6,8,10]
+
+// 4. Write a function to find duplicate values in an array
+// a. Example : arr = [1, 2, 2, 2, 3, 3, 4, 5, 5] → [2, 3, 5]
+
+// 5. Write a function to find the difference in 2 given array
+// a. Example : arr1 = [1, 2, 3, 4, 5], arr2 = [3, 4, 5, 6, 7] → [1, 2, 6, 7] 
